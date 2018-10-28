b7224137bf36a0f2195f701c7cfc42686edc4df3
pat <pmullot@gmail.com>
2018-10-28 15:29:41 +0100

    removing firebase refrences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
@@ -33,10 +28,6 @@ import { PubsubService } from './pubsub.service';
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule,
-    AngularFirestoreModule,
-    AngularFireDatabaseModule,
   ],
   providers: [FirebaseService, PubsubService],
   bootstrap: [AppComponent]
